31f094958f99c72fdad70034d9bac0fead2d2ad9
psfer07 <102543836+psfer07@users.noreply.github.com>
2024-03-31 20:30:36 +0200

    Eliminado app.js
    Creación a modo de ejemplo de escenario básico

--- app.js
+++
@@ -1,9 +0,0 @@
-var canvas = document.getElementById('aco_render');
-var ctx = canvas.getContext('2d');
-
-// Agrega un event listener para el evento mousedown
-canvas.addEventListener('mousedown', function(event) {
-    var x = event.offsetX;
-    var y = event.offsetY;
-    alert('Coordenadas del clic: x = ' + x + ', y = ' + y);
-});
