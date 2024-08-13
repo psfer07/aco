426d29be07d4f97dcdedb434f9f87d61488e17ef
psfer07 <102543836+psfer07@users.noreply.github.com>
2024-08-13 17:06:43 +0200

    Reworked script
    Changed parts of the script for better ones
    Added some functions to simulate the ants behavior
    Removed useless code

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 window.onload = function () {
     const canvas = document.getElementById("canvas_render");
     const paint = canvas.getContext("2d");
-    const antsRange = document.getElementById("ants");
-    const ants_value = document.getElementById("ants_value");
     const startButton = document.getElementById("start");
     const rho = document.getElementById("rho");
     const alpha = document.getElementById("alpha");
@@ -11,7 +9,9 @@ window.onload = function () {
     const gridWidth = 360;
     const gridHeight = 400;
     const cellSize = 2;
+    let start;
     let grid = [];
+    let visited = [];
     let properties = { // Each cell will have these properties by default
         color: "#ccc",
         pheromone: 1.0
@@ -31,7 +31,7 @@ window.onload = function () {
         grid.push(cols);
     }
 
-    function ant_move(x, y, alpha, beta) {
+    function ant_move(x, y, visited, alpha, beta) {
         function calcCost(pheromone) {
             let sum = 0;
             let distances = [];
@@ -45,6 +45,7 @@ window.onload = function () {
                     distances.push(Math.sqrt(2)); // If it is a diagonal
                 }
             }
+
             for (let i = 0; i < pheromone.length; i++) {
                 costs.push(Math.pow(pheromone[i], alpha) * Math.pow(distances[i], beta));
             }
@@ -53,7 +54,6 @@ window.onload = function () {
             const costSum = costs.reduce((sum, value) => sum + value, 0);
             const probabilities = costs.map(cost => cost / costSum);
 
-            console.log(costSum);
             for (let i = 0; i < probabilities.length; i++) {
                 sum += probabilities[i];
                 if (Math.random() < sum) {
@@ -66,20 +66,22 @@ window.onload = function () {
             { x: 0, y: 1 }, // Down
             { x: -1, y: 0 }, // Left
             { x: 1, y: 0 }, // Right
-            { x: 1, y: 1 }, // Up-left
+            { x: -1, y: -1 }, // Up-left
             { x: 1, y: -1 }, // Up-right
             { x: -1, y: 1 }, // Down-left
             { x: 1, y: 1 }  // Down-right
         ];
-        total_pheromone = [];
+        let total_pheromone = [];
         for (const direction of directions) {
             const inX = x + direction.x;
             const inY = y + direction.y;
+            if (visited.some(cell => cell.x === x && cell.y === y)) {
+                grid[x][y].pheromone = 0;
+            }
             total_pheromone.push(grid[inX][inY].pheromone);
         };
-        const path = calcCost(total_pheromone);
-
-        const movedTo = { x: x + directions[path].x, y: y + directions[path].y, z: path };
+        const path = calcCost(total_pheromone, visited);
+        const movedTo = { x: x + directions[path].x, y: y + directions[path].y };
         return movedTo;
     }
     function setColor(x, y, color) {
@@ -94,51 +96,92 @@ window.onload = function () {
             }
         }
     }
-    function drawCells(x, y) {
-        // Floor
-        setColor([2, gridWidth - 3], [2, gridHeight - 3], "#ccc");
-        // Walls
-        walls.horz.positions.forEach(wall => {
-            setColor([wall.x, wall.x + walls.horz.width - 1], [wall.y, wall.y + walls.horz.height - 1], walls.color);
-        });
-        walls.vert.positions.forEach(wall => {
-            setColor([wall.x, wall.x + walls.vert.width - 1], [wall.y, wall.y + walls.vert.height - 1], walls.color);
-        });
-        // Windows
-        windows.positions.forEach(window => {
-            setColor([window.x, window.x + windows.width - 1], [window.y, window.y + windows.height - 1], windows.color);
-        });
-        // Door
-        setColor([door.x, door.x + door.width - 1], [door.y, door.y + door.height - 1], door.color);
-        // Pillars
-        obstacles.pillars.positions.forEach(pillar => {
-            setColor([pillar.x, pillar.x + obstacles.pillars.width - 1], [pillar.y, pillar.y + obstacles.pillars.height - 1], obstacles.pillars.color);
-        });
-        // Teacher's table
-        const Ttable = obstacles.teacher_table
-        setColor([Ttable.x, Ttable.x + Ttable.width - 1], [Ttable.y, Ttable.y + Ttable.height - 1], Ttable.color);
-        // Tables
-        const table = obstacles.tables;
-        for (let sector = 0; sector < table.sectors.count; sector++) {
-            const sectorX = 2.8 * table.margins.marginsector * sector + table.margins.marginsector * 0.5;
-            for (let col = 0; col < table.sectors.cols; col++) {
-                const tableX = sectorX + col * (table.width + table.margins.marginX);
-                for (let row = 0; row < table.sectors.rows; row++) {
-                    const tableY = 90 + row * (table.margins.marginY + table.height);
-                    setColor([tableX, tableX + table.width], [tableY, tableY + table.height], table.color);
+    function drawCells(x, y, noReload) {
+        if (!noReload) {
+            // Floor
+            setColor([2, gridWidth - 3], [2, gridHeight - 3], "#ccc");
+            // Walls
+            walls.horz.positions.forEach(wall => {
+                setColor([wall.x, wall.x + walls.horz.width - 1], [wall.y, wall.y + walls.horz.height - 1], walls.color);
+            });
+            walls.vert.positions.forEach(wall => {
+                setColor([wall.x, wall.x + walls.vert.width - 1], [wall.y, wall.y + walls.vert.height - 1], walls.color);
+            });
+            // Windows
+            windows.positions.forEach(window => {
+                setColor([window.x, window.x + windows.width - 1], [window.y, window.y + windows.height - 1], windows.color);
+            });
+            // Door
+            setColor([door.x, door.x + door.width - 1], [door.y, door.y + door.height - 1], door.color);
+            // Pillars
+            obstacles.pillars.positions.forEach(pillar => {
+                setColor([pillar.x, pillar.x + obstacles.pillars.width - 1], [pillar.y, pillar.y + obstacles.pillars.height - 1], obstacles.pillars.color);
+            });
+            // Teacher's table
+            const Ttable = obstacles.teacher_table
+            setColor([Ttable.x, Ttable.x + Ttable.width - 1], [Ttable.y, Ttable.y + Ttable.height - 1], Ttable.color);
+            // Tables
+            const table = obstacles.tables;
+            for (let sector = 0; sector < table.sectors.count; sector++) {
+                const sectorX = 2.8 * table.margins.marginsector * sector + table.margins.marginsector * 0.5;
+                for (let col = 0; col < table.sectors.cols; col++) {
+                    const tableX = sectorX + col * (table.width + table.margins.marginX);
+                    for (let row = 0; row < table.sectors.rows; row++) {
+                        const tableY = 90 + row * (table.margins.marginY + table.height);
+                        setColor([tableX, tableX + table.width], [tableY, tableY + table.height], table.color);
+                    }
                 }
             }
-        }
-        // Where the user clicks
-        if (x, y) {
-            setColor([x - 2, x + 2], [y - 2, y + 2], "red");
-        }
+            // Where the user clicks
+            if (x, y) {
+                setColor([x - 2, x + 2], [y - 2, y + 2], "red");
+            }
+        } else { setColor(x, y, "green"); }
+
         // Paints each cell with its corresponding color
         for (let i = 0; i < grid.length; i++) {
             for (let j = 0; j < grid[i].length; j++) {
                 paint.fillStyle = grid[i][j].color;
                 paint.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
             }
+        }
+    }
+    function isFloor(x, y) {
+        let state = true;
+        for (let i = x - 2; i <= x + 2; i++) {
+            for (let j = y - 2; j <= y + 2; j++) {
+                if (grid[i][j].color != "#ccc") {
+                    state = false;
+                }
+            }
+        }
+        return state;
+    }
+    function pheromoneEvaporation(rho) {
+        for (let i = 0; i < gridWidth; i++) {
+            for (let j = 0; j < gridHeight; j++) {
+                grid[i][j].pheromone = (1 - rho) * grid[i][j].pheromone;
+            }
+        }
+    }
+    function antStart(initial, alpha, beta, rho, deposit) {
+        let x = initial.x;
+        let y = initial.y;
+        let k = 0;
+        do {
+            let ant = ant_move(x, y, visited, alpha, beta);
+            drawCells(ant.x, ant.y, 1);
+            visited.push({ x: ant.x, y: ant.y });
+            console.log("La hormiga se ha desplazado a la celda:", visited);
+            pheromoneEvaporation(rho);
+            x = ant.x;
+            y = ant.y;
+            k++;
+        } while (k < 20)
+        for (const visit of visited) {
+            console.log(alpha, beta, rho, deposit)
+            grid[visit.x][visit.y].pheromone += deposit;
+            console.log(visit, grid[visit.x][visit.y].pheromone)
         }
     }
 
@@ -218,31 +261,25 @@ window.onload = function () {
     drawCells();
 
     canvas.addEventListener("click", function (event) {
+        visited = [];
         startButton.removeAttribute("disabled");
         startButton.classList.remove("disabled");
         const rect = canvas.getBoundingClientRect();
-        const cellX = Math.floor((event.clientX - rect.left) / cellSize);
-        const cellY = Math.floor((event.clientY - rect.top) / cellSize);
+        start = { x: Math.floor((event.clientX - rect.left) / cellSize), y: Math.floor((event.clientY - rect.top) / cellSize) };
 
-        switch (grid[cellX][cellY].color) {
-            case "#ccc":
-                console.log("Coordenadas iniciales:", cellX, cellY);
-                drawCells(cellX, cellY);
-                let ant = ant_move(cellX, cellY, alpha.value, beta.value);
-                console.log("La hormiga se ha desplazado a la celda:", ant.x, ant.y, ant.z);
-                break;
-            case "red":
-                alert("Por favor, seleccione otra ubicación o pulse el botón de marcar el punto de partida")
-                break;
-            default:
-                alert("No puedes empezar ahí. Haz clic en el suelo de la habitación");
-                break;
+        if (isFloor(start.x, start.y)) {
+            console.log("Coordenadas establecidas en:", start.x, start.y);
+            drawCells(start.x, start.y);
+        } else {
+            if (grid[start.x][start.y].color === "red") {
+                alert("Por favor, seleccione otro punto o inicie la simulación.")
+            } else { alert("No puedes empezar ahí. Haz clic en el suelo de la habitación."); }
         }
-    });
-    antsRange.addEventListener("change", function () {
-        ants_value.textContent = antsRange.value
     });
     document.getElementById("reset").addEventListener("click", function () {
         drawCells();
+    });
+    startButton.addEventListener("click", function () {
+        if (start) { antStart(start, Number(alpha.value), Number(beta.value), Number(rho.value), Number(deposit.value)); }
     });
 };
